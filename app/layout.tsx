1444296975892353da489fb73f5de1499b6d8a6d
SANNNNN-123 <zuhairaziz95@yahoo.com>
2025-03-12 21:35:32 +0800

    add tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Press_Start_2P } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import "nes.css/css/nes.min.css";
 import "./nes-custom.css";
@@ -38,6 +39,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script 
+        src="https://cloud.umami.is/script.js" 
+        data-website-id="476b1269-cf98-4402-bff6-b865b2ce2119"
+        strategy="afterInteractive"
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${pressStart2P.variable} antialiased nes-style custom-cursor`}
       >
